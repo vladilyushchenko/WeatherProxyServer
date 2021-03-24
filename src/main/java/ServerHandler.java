b0f6578b9ecd2d7a5211264ba22dc0bdde12910d
vladilyushchenko <vladislavilyushchenko@gmail.com>
2021-03-24 11:32:03 +0300

    asynchrony added, cash fixed, integrationTests filled with assertions

--- src/main/java/ServerHandler.java
+++
@@ -1,7 +0,0 @@
-import java.io.IOException;
-import java.io.PrintWriter;
-interface ServerHandler {
-    String getRequest() throws IOException;
-    void startServer() throws IOException;
-    void sendGetResponse(String request, PrintWriter output);
-}
--- src/main/java/http_utilities/RequestType.java
+++
@@ -1,9 +0,0 @@
-package http_utilities;
-
-public enum RequestType {
-
-    GET,
-    GET_FAVICON,
-    INCORRECT
-
-}
